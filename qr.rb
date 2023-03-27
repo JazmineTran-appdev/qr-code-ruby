0708ff4a1c71fe6135c2f0f32ef02a03bc420af9
Jazmine Tran <jazminetra00@gmail.com>
2023-03-27 15:35:24 +0000

    added the code to make a wifi qr code

--- qr.rb
+++ qr.rb
@@ -11,6 +11,15 @@ user_input = gets.chomp.to_i
 if user_input == 1
   puts "\nEnter the URL that you would like to encode.\n"
   user_qr = gets.chomp
+
+elsif user_input == 2
+  puts "\nWhat is the name of the wifi network?\n"
+  wifi_name = gets.chomp
+
+  puts "\nWhat is the wifi password?\n"
+  wifi_pass = gets.chomp
+
+  user_qr = "WIFI:T:WPA;S:#{wifi_name};P:#{wifi_pass};;"
 end
 
 puts "\nWhat would you like to call the PNG?\n"
